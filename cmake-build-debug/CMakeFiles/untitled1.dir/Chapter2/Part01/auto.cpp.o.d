7a833ecfdc60a07cea61fc445ab8f68eb0b18c6a
이선우 <leesw1347@gmail.com>
2022-06-16 01:28:08 +0900

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/untitled1.dir/Chapter2/Part01/auto.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/untitled1.dir/Chapter2/Part01/auto.cpp.o: \
- /mnt/c/Users/lsw94/Desktop/CLion/untitled1/Chapter2/Part01/auto.cpp \
- /usr/include/stdc-predef.h
--- cmake-build-debug/CMakeFiles/untitled1.dir/Chapter2/Part02/2-07.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/untitled1.dir/Chapter2/Part02/2-07.cpp.o: \
- /mnt/c/Users/lsw94/Desktop/CLion/untitled1/Chapter2/Part02/2-07.cpp \
- /usr/include/stdc-predef.h
